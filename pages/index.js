10e465656104fbbd909c621a403ad80490e00e6e
rndexe <h_erande@yahoo.co.in>
2021-03-04 19:46:00 +0530

    change example blog

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,27 @@
 import Head from 'next/head'
+import Link  from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
 export default function Home({ allPostsData }) {
+  
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({ id, date, title, image, video, link }) => (
             <li className={utilStyles.listItem} key={id}>
               {title}
               <br />
-              {id}
-              <br />
-              {date}
+	      {image} 
+	      <br />
+		  {video}
+	      <br />
+		  {link}
             </li>
           ))}
         </ul>
